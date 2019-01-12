4daf3815046f36f0d81b21d5e985a11fbecfb958
John Barksdale <jdbarks@gmail.com>
2019-01-11 23:26:21 -0600

    Mocked up main function.  Added header function.

---
+++ program.py
@@ -0,0 +1,19 @@
+def print_the_header():
+    print('----------------------------------------')
+    print('             WEATHER APP')
+    print('----------------------------------------')
+    print()
+
+
+def main():
+    print_the_header()
+
+    zipcode = input('What zipcode do you want the weather for (64836) ')
+    print(zipcode)
+    # get html from web
+    # parse the html
+    # display for the forecast
+
+
+if __name__ == '__main__':
+    main()
